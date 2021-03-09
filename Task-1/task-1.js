94de6e184f179a6d8a4c067b9ef8b690dc60599a
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-03-09 22:54:00 +0200

    add solutions od 2 js tasks

---
+++ Task-1/task-1.js
@@ -0,0 +1,5 @@
+let myName = 'Ieva';
+let myAddress = 'Greece';
+let favoriteNumber = '3';
+
+console.log('Hello, my name is ' + myName + ', I live in ' + myAddress + ' and my favorite number is ' + favoriteNumber + '!');
---
+++ Task-2/task-2.js
@@ -0,0 +1,7 @@
+const cars = ['Vaz', 'Zaz', 'Gaz', 'Uaz', 'Maz'];
+
+console.log('5th car is: ' + cars[4]);
+console.log('4th car is: ' + cars[3]);
+console.log('3rd car is: ' + cars[2]);
+console.log('2nd car is: ' + cars[1]);
+console.log('1st car is: ' + cars[0]);
